f1b26fd76c740ebc70d55b32541e0bc91bbcdbf7
Jeff Purvis <jpurvis00@gmail.com>
2023-11-06 14:18:26 -0500

    adding solution for ComputeAverage

---
+++ Arrays/ComputeAverage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CodeStepByStep_CSharp.Arrays
+{
+    //Write a method named ComputeAverage that computes and returns the mean
+    //of all elements in an array of integers.For example, if an array named
+    //a contains[1, -2, 4, -4, 9, -6, 16, -8, 25, -10], then the call of ComputeAverage(a) should return 2.5.
+
+    //Constraints: You may assume that the array contains at least one element.
+    //Your method should not modify the elements of the array.
+
+    internal class ComputeAverage
+    {
+        public static void RunComputeAverage(int[] a) 
+        { 
+            double sum = 0;
+            
+            foreach (var item in a)
+            {
+                sum += item;
+            }
+
+            var mean = sum / a.Length;
+
+            Console.WriteLine($"mean: {mean}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -20,5 +20,6 @@ using CodeStepByStep_CSharp.Arrays;
 //ArrayMystery.Mystery(new int[] { 7, 3, 2, 0, 5 });
 //Banish.BanishArrayNumbersRefactored(new int[] { 42, 3, 9, 42, 42, 0, 42, 9, 42, 42, 17, 8, 2222, 4, 9, 0, 1 }, new int[] { 42, 2222, 9 });
 //CollapsePairs.RunCollapsePairs(new int[] { 7, 2, 8, 9, 4, 22, 7, 1, 9, 10 });
-Collapse.RunCollapse(new int[] { 7, 2, 8, 9, 4, 13, 7, 1, 9, 10 });
-Collapse.RunCollapse(new int[] { 7, 2, 8, 9, 4, 13, 7, 1, 9 });
+//Collapse.RunCollapse(new int[] { 7, 2, 8, 9, 4, 13, 7, 1, 9, 10 });
+//Collapse.RunCollapse(new int[] { 7, 2, 8, 9, 4, 13, 7, 1, 9 });
+ComputeAverage.RunComputeAverage(new int[] { 1, -2, 4, -4, 9, -6, 16, -8, 25, -10 });
