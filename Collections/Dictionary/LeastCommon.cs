2f703dacfd264cb33c02edd94adf5b1cb6c8162c
Jeff Purvis <jpurvis00@gmail.com>
2024-03-11 11:17:59 -0400

    test

--- Collections/Dictionary/LeastCommon.cs
+++ Collections/Dictionary/LeastCommon.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+//Write a method LeastCommon that accepts a Dictionary whose keys are strings
+//and whose values are integers as a parameter and returns the integer value
+//that occurs the fewest times in the dictionary. For example, if a dictionary
+//named m contains {{"Alyssa", 22}, { "Char", 25}, { "Dan", 25}, { "Jeff", 20},
+//{ "Kasey", 20}, { "Kim", 20}, { "Mogran", 25}, { "Ryan", 25}, { "Stef", 22}},
+//the call of LeastCommon(m) returns 22. If there is a tie, return the smaller
+//integer value. If the dictionary is empty, throw an ArgumentException.
 
 namespace CodeStepByStep_CSharp.Collections.Dictionary
 {
-    internal class LeastCommon
+    public class LeastCommon
     {
     }
 }
